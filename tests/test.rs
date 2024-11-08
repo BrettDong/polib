449383a07f2e05885ac1774d782074963f6f7142
April is a Rabbit <16049823+LittleSaya@users.noreply.github.com>
2024-11-08 20:03:43 +0800

    fix sort test

--- tests/test.rs
+++ tests/test.rs
@@ -41,6 +41,45 @@ fn validate_catalog(catalog: &Catalog) {
     }
 }
 
+fn validate_catalog_sort(catalog: &Catalog) {
+    assert_eq!(catalog.count(), 4);
+    let mut index: usize = 0;
+    for message in catalog.messages() {
+        if index == 0 {
+            assert!(message.msgctxt().is_none());
+            assert_eq!(message.msgid(), "Book");
+            assert!(message.is_plural());
+            assert!(message.is_translated());
+            assert_eq!(message.msgid_plural().unwrap_or("None?"), "Books");
+            assert_eq!(
+                message
+                    .msgstr_plural()
+                    .unwrap_or(&vec![String::from("None?")])[0],
+                "Translated\n_\nBook"
+            );
+        } else if index == 1 {
+            assert_eq!(message.msgctxt().unwrap_or("None?"), "Context");
+            assert_eq!(message.msgid(), "Good");
+            assert!(message.is_singular());
+            assert!(message.is_translated());
+            assert_eq!(message.msgstr().unwrap_or("None?"), "\"\n\"123");
+        } else if index == 2 {
+            assert!(message.msgctxt().is_none());
+            assert_eq!(message.msgid(), "Hello");
+            assert!(message.is_singular());
+            assert!(message.is_translated());
+            assert_eq!(message.msgstr().unwrap_or("None?"), "Translated_Hello");
+        } else if index == 3 {
+            assert!(message.msgctxt().is_none());
+            assert_eq!(message.msgid(), "Untranslated");
+            assert!(!message.is_translated());
+            assert_eq!(message.msgstr().unwrap_or("None?"), "");
+        }
+
+        index += 1;
+    }
+}
+
 fn feed_test_po() -> Vec<u8> {
     let path = Path::new("./tests/sample.po");
     let text = std::fs::read_to_string(path).unwrap();
@@ -72,5 +111,5 @@ fn po_round_trip_sort() {
     })).unwrap();
     let po_bytes = writer.into_inner().unwrap();
     let catalog_2 = po_file::parse_from_reader(&*po_bytes).unwrap();
-    validate_catalog(&catalog_2);
+    validate_catalog_sort(&catalog_2);
 }
